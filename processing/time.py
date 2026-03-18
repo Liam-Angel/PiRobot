a0f0e1b013ae8f3558854e1528d6919bc0d65d5c
Liam <0622557@schoolsnet.act.edu.au>
2026-03-18 18:42:58 +1100

    chNGE

--- processing/time.py
+++ processing/time.py
@@ -24,23 +24,30 @@ while True:
   contours, hierarchy = cv.findContours(image,cv.RETR_EXTERNAL,cv.CHAIN_APPROX_SIMPLE)
   cv.drawContours(image, contours, -1, (255,255,255),2) 
   imageC = cv.cvtColor(image, cv.COLOR_BGR2RGB)
+  rects = [cv.minAreaRect(c) for c in contours if cv.contourArea(c) > 100]
+
+  linediff = 180
+  pair = (None, None)
   
 
   if len(contours) > 2:  
    
-   for contour in contours:
-    cnt = contour
+   for cnt in contours:
     M = cv.moments(cnt)
     perimeter = cv.arcLength(cnt, True)
     epsilon = 10 * perimeter
     approx_contour = cv.approxPolyDP(cnt, epsilon, True)
     x,y,w,h = cv.boundingRect(cnt)
-    contours = sorted(contours)
+    rect = cv.minAreaRect(cnt)
 
     if perimeter >100 and h >  50:
       #print(perimeter)
-      cv.rectangle(imageC,(x,y),(x+w,y+h), (0, 255, 0),2)
-      print(h)
+      rows, cols = imageC.shape[:2]
+      [vx, vy, x, y,] = cv.fitLine(cnt, cv.DIST_L2,0,0.01,0,0.01)
+      lefty = int((-x*vy/vx)+y)
+      righty = int(((cols-x)*vy/vx)+y)
+      #cv.rectangle(imageC,(x,y),(x+w,y+h), (0, 255, 0),2)
+      cv.line(imageC,(cols-1,righty),(0,lefty),(0,255,0),2)
 
    cv.imshow('imageC', imageC)
 
