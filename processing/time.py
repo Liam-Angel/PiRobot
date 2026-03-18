879c004154db09fc9c61993d9dd66e36bdf75bdf
Liam <0622557@schoolsnet.act.edu.au>
2026-03-18 17:02:01 +1100

    new

--- processing/time.py
+++ processing/time.py
@@ -10,9 +10,10 @@ cam = cv.VideoCapture(0)
 
 while True: 
   check, frame = cam.read()
-  image = cv.resize(frame,(320,240))
+  img = cv.resize(frame,(320,240))
 
-  image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+  image = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+  image = cv.GaussianBlur(image, (11, 11), sigmaX=0)
 
   # Threshold         120 is threshold, 255 is what we assign if it is below this
   _, image = cv.threshold(image, 150, 255, cv.THRESH_BINARY)
@@ -22,27 +23,27 @@ while True:
 
   contours, hierarchy = cv.findContours(image,cv.RETR_EXTERNAL,cv.CHAIN_APPROX_SIMPLE)
   cv.drawContours(image, contours, -1, (255,255,255),2) 
+  imageC = cv.cvtColor(image, cv.COLOR_BGR2RGB)
 
   if len(contours) > 0:  
-   cnt = contours[0]
-   M = cv.moments(cnt)
+   for contour in contours:
+    cnt = contour
+    M = cv.moments(cnt)
+    perimeter = cv.arcLength(cnt, True)
+    epsilon = 100 * perimeter
+    approx_contour = cv.approxPolyDP(cnt, epsilon, True)
 
-   perimeter = cv.arcLength(cnt, True)
+
+    x,y,w,h = cv.boundingRect(cnt)
+    cv.rectangle(imageC,(x,y),(x+w,y+h), (0, 255, 0),2)
+
+   cv.imshow('imageC', imageC)
+
+
+
+
   
-   print( perimeter )
-
-   for perimiter in perimiter:
-
-
-
-   imageC = cv.cvtColor(image, cv.COLOR_BGR2RGB)
-
-   x,y,w,h = cv.boundingRect(cnt)
-   cv.rectangle(imageC,(x,y),(x+w,y+h), (0, 255, 0),2)
-
   
-  
-  cv.imshow('imageC', imageC)
 
   key = cv.waitKey(1)
   if key == 27: # exit on ESC
